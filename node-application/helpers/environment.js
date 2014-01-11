875e78181624c3b60e46ffbdcf7cc8bf21c98ace
Paulo McNally <paulomcnally@gmail.com>
2014-01-11 05:34:35 -0600

    added environment file

---
+++ node-application/helpers/environment.js
@@ -0,0 +1,40 @@
+/**
+ * @description Set environment var
+ * @type {exports}
+ */
+
+exports.init = function(){
+
+    switch ( __dirname ){
+        case '/sites/my-application':
+
+            process.env.NODE_ENV = 'production';
+            process.env.PORT = 8080;
+            process.env.NEW_RELIC_APP_NAME = 'my-application';
+            process.env.NEW_RELIC_ENABLED = true;
+
+            break;
+
+        case '/sites/betas/my-application':
+
+            process.env.NODE_ENV = 'beta';
+            process.env.PORT = 8081;
+            process.env.NEW_RELIC_APP_NAME = 'my-application-' + process.env.NODE_ENV;
+            process.env.NEW_RELIC_ENABLED = true;
+
+            break;
+
+        default:
+
+            process.env.NODE_ENV = 'development';
+            process.env.PORT = 8082;
+            process.env.NEW_RELIC_APP_NAME = 'my-application-' + process.env.NODE_ENV;
+            process.env.NEW_RELIC_ENABLED = false;
+    }
+
+    console.log( 'Mode: ' + process.env.NODE_ENV );
+    console.log( 'Port: ' + process.env.PORT );
+    console.log( 'Name: ' + process.env.NEW_RELIC_APP_NAME );
+    console.log( 'Newrelic: ' + process.env.NEW_RELIC_ENABLED );
+
+}
