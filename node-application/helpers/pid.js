5f2b0eff23299477a618747985c23e0097176fdd
Paulo McNally <paulomcnally@gmail.com>
2014-01-11 04:24:48 -0600

    add node application

---
+++ node-application/helpers/pid.js
@@ -0,0 +1,56 @@
+/**
+ * @description Create and remove pid file.
+ * @type {exports}
+ */
+
+var fs = require('fs');
+var path = require('path');
+
+function getPidPath( ){
+
+    var pidPath = path.resolve( __dirname, '../pids/pid.pid' );
+
+    return pidPath;
+
+}
+
+exports.make = function( ){
+
+    // on application exit remove pid file
+    process.on('exit', function() {
+
+        fs.unlinkSync( getPidPath( ) );
+
+    });
+
+    // kill pid and remove file only if file exist
+    if( fs.existsSync( getPidPath( ) ) ){
+
+        var oldPid = fs.readFileSync( getPidPath( ),'utf8' );
+
+        process.kill( oldPid );
+
+        fs.unlinkSync( getPidPath( ) );
+
+    }
+
+    // pids directory path
+    var directory = path.resolve( __dirname, '../pids');
+
+    // create dir if not exit
+    if( !fs.existsSync( directory ) ){
+
+        fs.mkdirSync( directory );
+
+    }
+
+    // Create pid file
+    setTimeout(function(){
+        // create pid
+        var pidfile = fs.openSync(getPidPath( ), "w");
+        fs.writeSync(pidfile, process.pid);
+        fs.closeSync(pidfile);
+
+    },3000);
+
+}
